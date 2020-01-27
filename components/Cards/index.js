254f2d6e1c12ba84f0f34d40ceacbda32bda522c
DrewCDoll <drwcdoll@gmail.com>
2020-01-26 22:52:00 -0500

    mvp achieved

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,70 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+
+
+axios
+    .get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(resp => {
+        console.log(resp);
+        const jArticles = resp.data.articles.javascript;
+            jArticles.forEach(art => {
+            const newArtCard = createCard(art);
+            document.querySelector('.cards-container').appendChild(newArtCard);
+            })
+        const bArticles = resp.data.articles.bootstrap;
+            bArticles.forEach(art => {
+            const newArtCard = createCard(art);
+            document.querySelector('.cards-container').appendChild(newArtCard);
+            })
+        const tArticles = resp.data.articles.technology;
+            tArticles.forEach(art => {
+            const newArtCard = createCard(art);
+            document.querySelector('.cards-container').appendChild(newArtCard);
+            })
+        const jqArticles = resp.data.articles.jquery;
+            jqArticles.forEach(art => {
+            const newArtCard = createCard(art);
+            document.querySelector('.cards-container').appendChild(newArtCard);
+            })
+        const nArticles = resp.data.articles.node;
+            nArticles.forEach(art => {            
+            const newArtCard = createCard(art);
+            document.querySelector('.cards-container').appendChild(newArtCard);
+            })
+        })
+
+
+
+
+const createCard = (artObj) => {
+    //create elements
+    const artCard = document.createElement('div');
+    const artHead = document.createElement('div');
+    const artAuthor = document.createElement('div');
+    const artAuthImg = document.createElement('div');
+    const artAuthImgSrc = document.createElement('img');
+    const artAuthName = document.createElement('span');
+
+    //set classes
+    artCard.classList.add('card');
+    artHead.classList.add('headline');
+    artAuthor.classList.add('author');
+    artAuthImg.classList.add('img-container');
+
+    //set tree
+    artCard.appendChild(artHead);
+    artCard.appendChild(artAuthor);
+    artAuthor.appendChild(artAuthImg);
+    artAuthImg.appendChild(artAuthImgSrc);
+    artAuthor.appendChild(artAuthName);
+
+    //add content
+    artHead.textContent = artObj.headline;
+    artAuthImgSrc.src = artObj.authorPhoto;
+    artAuthName.textContent = `By ${artObj.authorName}`;
+
+    return artCard;
+}
+
